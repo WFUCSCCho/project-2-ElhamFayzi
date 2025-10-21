545e2f4f08250543a5abc0848263b51007698be4
ElhamFayzi <fayze24@wfu.edu>
2025-10-21 00:13:33 -0400

    Created the generic BST.java, and completed the implementation of AVLTree.java. Added and completed Player.java, which is a custom class for dataset entries. Completed parsing functionality and implementation of 4 separate trees in proj2.java.

--- src/Proj2.java
+++ src/Proj2.java
@@ -1,11 +1,35 @@
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
 
 public class Proj2 {
+    public static void populateBST(BST<Player> BST, ArrayList<Player> players) {
+        for (Player player : players) {
+            BST.insert(player);
+        }
+    }
+
+    public static void populateAVL(AVLTree<Player> AVLTree, ArrayList<Player> players) {
+        for (Player player : players) {
+            AVLTree.insert(player);
+        }
+    }
+
+    public static void searchForEachPlayersBST(BST<Player> BST, ArrayList<Player> players) {
+        for (Player player : players) {
+            BST.search(player);
+        }
+    }
+
+    public static void searchForEachPlayersAVL(AVLTree<Player> AVLTree, ArrayList<Player> players) {
+        for (Player player : players) {
+            AVLTree.contains(player);
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
         // Use command line arguments to specify the input file
         if (args.length != 2) {
@@ -28,6 +52,45 @@ public class Proj2 {
         inputFileNameScanner.nextLine();
 
 	// FINISH ME
+        ArrayList<Player> originalPlayersList = new ArrayList<Player>();
+        ArrayList<Player> copyPlayersList = new ArrayList<Player>();
+
+        for (int i = 0; i < numLines; i++) {
+
+            String[] line = inputFileNameScanner.nextLine().toLowerCase().split(",");
+            Player player;
+            try {
+                player = new Player(line);
+                originalPlayersList.add(player);
+                copyPlayersList.add(player);
+            } catch (Exception e) {
+                System.out.println("Line Number " + (i + 1) + " has an invalid format.");
+            }
+        }
+
+        //============================
+        Collections.sort(copyPlayersList);
+
+        BST<Player> sortedBST = new BST<Player>();
+        populateBST(sortedBST, copyPlayersList);
+
+        AVLTree<Player> sortedAVLTree = new AVLTree<Player>();
+        populateAVL(sortedAVLTree, copyPlayersList);
+
+        //============================
+        Collections.shuffle(copyPlayersList);
+        BST<Player> randomBST = new BST<Player>();
+        populateBST(randomBST, copyPlayersList);
+
+        AVLTree<Player> randomAVLTree = new AVLTree<Player>();
+        populateAVL(randomAVLTree, copyPlayersList);
+        //=============================
+
+        searchForEachPlayersBST(sortedBST, originalPlayersList);
+        searchForEachPlayersAVL(sortedAVLTree, originalPlayersList);
+        searchForEachPlayersBST(randomBST, originalPlayersList);
+        searchForEachPlayersAVL(randomAVLTree, originalPlayersList);
+
 
     }
 }
